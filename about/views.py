6a8bfd9001f8e3817b82677e75f3878b62e52ea9
Charlotte Galpin <charlotte.galpin92@gmail.com>
2024-12-19 15:42:27 +0000

    added comment CRUD

--- about/views.py
+++ about/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
+from django.contrib import messages
 from .models import About
+from .forms import CollaborateForm
 
 # Create your views here.
     
@@ -8,20 +10,31 @@ Renders the About Page
 """
 def about_me(request):
 
-   """
-   selects a single about entry (if multiple entries have been
-   added in the admin portal).
-   The objects.all selects all the information within that entry
-   so the title, body etc.
-   orders it in reverse and then selects the first entry
-   stores it in a variable called about
-   """
-   about = About.objects.all().order_by('-updated_on').first()
+    if request.method == "POST":
+        collaborate_form = CollaborateForm(data==request.POST)
+        if collaborate_form.is_valid():
+            collaborate_form.save()
+            message.add_message(request, messages.SUCCESS, 
+            "Collaboration request received! I endeavor to respond within 2 working days.")
+    """
+    selects a single about entry (if multiple entries have been
+    added in the admin portal).
+    The objects.all selects all the information within that entry
+    so the title, body etc.
+    orders it in reverse and then selects the first entry
+    stores it in a variable called about
+    """
+
+    about = About.objects.all().order_by('-updated_on').first()
+    collaborate_form = CollaborateForm()
    
-   return render(
-       request,
-       "about/about.html",
-       {"about": about},
+    return render(
+        request,
+        "about/about.html",
+        {
+        "about": about,
+        "collaborate_form": collaborate_form
+    },
     )
 
 """
--- static/js/script.js
+++
@@ -1,1 +0,0 @@
-console.log('print something to the console')
